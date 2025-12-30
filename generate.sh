250f02075c74fcabbae0081d8a90638c55d1301c
Filip Kopyt <filipkopyt14@gmail.com>
2025-12-30 18:53:20 +0100

    POP

--- generate.sh
+++ generate.sh
@@ -17,16 +17,18 @@ generate_batch() {
 }
 
 # 1. Małe taski (10-20), Małe grupy (2-5)
-generate_batch 15 20 5 10
+#generate_batch 15 20 5 10
 
 # 2. Średnie taski (50), Małe grupy (3)
-generate_batch 15 50 3 20
+#generate_batch 15 50 3 20
 
 # 3. Małe taski (20), Średnie grupy (10)
-generate_batch 15 20 10 20
+#generate_batch 15 20 10 20
 
 # 4. Średnie taski (50), Średnie grupy (10)
-generate_batch 15 50 10 50
+#generate_batch 15 50 10 50
 
 # 5. Wszystko duże (Taski 100, Grupy 20)
-generate_batch 15 100 20 100
+#generate_batch 15 100 20 100
+
+generate_batch 25 1000 200 1000
